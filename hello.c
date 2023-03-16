216710795ad4a6960efe8cca676693d658a54487
Eric Drouillard <eric.drouillard@gmail.com>
2023-03-16 16:51:24 +0000

    commit

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("Hello, world\n");
-    }
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s!\n", answer);
+}
     
