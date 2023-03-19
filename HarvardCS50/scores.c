94c959c5442e63af25ec53da193235bd2fd55934
Eric Drouillard <eric.drouillard@gmail.com>
2023-03-19 20:59:19 +0000

    refactor 1

--- HarvardCS50/scores.c
+++ HarvardCS50/scores.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <cs50.h>
+
+const int N = 3;
+
+float average(int array[]);
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores [N];
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
 
-    printf("My average is: %f\n", (score1 + score2 + score3) / (float) 3);
+    printf("Average: %f\n", average(scores));
+
+}
+
+float average (int array[])
+{
+    int sum = 0; 
+    for (int i = 0; i < N; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) N;
 }
